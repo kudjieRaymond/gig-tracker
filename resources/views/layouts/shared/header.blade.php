929512cdd56a302e9499a12ad89eade7f34025b7
Ray Kudjie <rkudjie@gmail.com>
2019-11-01 22:42:22 +0000

    update docker compos and readme

--- resources/views/layouts/shared/header.blade.php
+++ resources/views/layouts/shared/header.blade.php
@@ -47,11 +47,11 @@
 							<!-- ============================================================== -->
 							<!-- Search -->
 							<!-- ============================================================== -->
-							<li class="nav-item search-box"> <a class="nav-link waves-effect waves-dark" href="javascript:void(0)"><i class="ti-search"></i></a>
+							{{-- <li class="nav-item search-box"> <a class="nav-link waves-effect waves-dark" href="javascript:void(0)"><i class="ti-search"></i></a>
 									<form class="app-search position-absolute">
 											<input type="text" class="form-control" placeholder="Search &amp; enter"> <a class="srh-btn"><i class="ti-close"></i></a>
 									</form>
-							</li>
+							</li> --}}
 					</ul>
 					<!-- ============================================================== -->
 					<!-- Right side toggle and nav items -->
